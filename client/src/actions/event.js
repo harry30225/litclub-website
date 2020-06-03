3640b5bf55700bc219bac41d74db61fd00354ae0
Harshit Sharma <harshit.s@students.iiit.ac.in>
2020-06-03 12:46:57 +0530

    Frontend of Eventdate and its backend functionality

--- client/src/actions/event.js
+++ client/src/actions/event.js
@@ -1,46 +1,44 @@
-import axios from 'axios';
-import {
-  GET_EVENTS,
-  EVENTS_ERROR,
-  ADD_EVENT
-} from './types';
+import axios from "axios";
+import { GET_EVENTS, EVENTS_ERROR, ADD_EVENT } from "./types";
 
 // Get events
-export const getEvents = () => async dispatch => {
-    try {
-      const res = await axios.get('/api/event');
-  
-      dispatch({
-        type: GET_EVENTS,
-        payload: res.data
-      });
-    } catch (err) {
-      dispatch({
-        type: EVENTS_ERROR,
-        payload: { msg: "error" }
-      });
-    }
+export const getEvents = () => async (dispatch) => {
+  try {
+    const res = await axios.get("/api/event");
+
+    dispatch({
+      type: GET_EVENTS,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: EVENTS_ERROR,
+      payload: { msg: "error" },
+    });
+  }
 };
 
 // add events
-export const addEvent = (name,venue,description) => async dispatch => {
-    const config = {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    };
-    const body = JSON.stringify({ name, venue,description });
-    try {
-      const res =await axios.post(`/api/event/`,body,config);
-  
-      dispatch({
-        type: ADD_EVENT,
-        payload: res.data
-      });
-    } catch (err) {
-      dispatch({
-        type: EVENTS_ERROR,
-        payload: { msg: "error" }
-      });
-    }
+export const addEvent = (name, venue, description, eventdate) => async (
+  dispatch
+) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
   };
+  const body = JSON.stringify({ name, venue, description, eventdate });
+  try {
+    const res = await axios.post(`/api/event/`, body, config);
+
+    dispatch({
+      type: ADD_EVENT,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: EVENTS_ERROR,
+      payload: { msg: "error" },
+    });
+  }
+};
