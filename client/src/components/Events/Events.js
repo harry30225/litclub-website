2196a1d4137659c1fec825260897d7bb78ab876a
jasshan29goel <goeljashan29@gmail.com>
2020-06-07 21:02:12 +0530

    breadcrumb added on events page

--- client/src/components/Events/Events.js
+++ client/src/components/Events/Events.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getEvents } from '../../actions/event';
@@ -13,6 +14,21 @@ const Events = ({ getEvents, event: { events, loading } }) => {
 
   return !loading && (
     <div>
+      <section class="breadcrumb-section">
+          <div class="container">
+              <div class="row">
+                  <div class="col-lg-12">
+                      <div class="breadcrumb-text">
+                          <h2>Our Events</h2>
+                          <div class="bt-option">
+                              <Link to="/">Home</Link>
+                              <span>Events</span>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </section>
       {events.map(event => (
         <EventElement name={event.name} date={event.date} description={event.description} venue={event.venue} eventdate={event.eventdate} key={event._id} />
       ))}
